6675146f6ae9e25b850296098c80904feb2e8b01
GustavoSMafra <mafraguh@gmail.com>
2024-04-12 15:20:36 -0300

    Add function to authenticate jwt token

---
+++ utils/userUtils.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const secretKey = process.env.SECRET_KEY;
+
+const User = require('../models/User');
+
+function autenticarUsuario(req, res, next) {
+    const token = req.cookies.authToken;
+
+    if (token) {
+        jwt.verify(token, secretKey, async (err, decoded) => {
+            if (err) {
+                res.clearCookie('authToken');
+                res.redirect('user/login');
+            }
+
+            const user = await User.findOne({
+                where: { id: decoded.id },
+                attributes: ['id', 'name'],
+                raw: true
+            });
+
+            if(user) {
+                req.user = user;
+                next();
+            } else {
+                res.clearCookie('authToken');
+                res.redirect('user/login');
+            }
+
+        });
+    } else {
+        res.redirect('user/login');
+    }
+}
+
+module.exports = {
+    autenticarUsuario
+};
