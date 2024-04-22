56bc48dfed6bafdb5e540dd58741b81ff29888b4
GustavoSMafra <mafraguh@gmail.com>
2024-04-22 15:05:14 -0300

    Change style to tailwind classes

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -5,7 +5,7 @@ const Task = require('../models/Task');
 module.exports = class SiteController {
     
     static async homeView(req, res) {
-        res.render('site/home', {user: req.user});
+        res.redirect('/task/');
     }
 
 }
