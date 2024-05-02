f21ed7c3ec7992c8c56b3e484cb183e3289c3b06
GustavoSMafra <mafraguh@gmail.com>
2024-05-02 13:16:39 -0300

    Added postgres and change some scripts

--- index.js
+++ index.js
@@ -27,21 +27,14 @@ const siteRoutes = require("./routes/siteRoutes");
 const userRoutes = require("./routes/userRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
-const commentRoutes = require("./routes/commentRoutes");
 const checklistRoutes = require("./routes/checklistRoutes");
 
 app.use('/', siteRoutes);
 app.use('/user', userRoutes);
 app.use('/task', taskRoutes);
 app.use('/category', categoryRoutes);
-app.use('/comment', commentRoutes);
 app.use('/checklist', checklistRoutes);
 
 conn.sync().then(() => {
-    app.listen(3000);
+    app.listen(Number(process.env.PORT));
 }).catch(err => console.log(err));
-
-
-// conn.sync({force: true}).then(() => {
-//     app.listen(3000);
-// }).catch(err => console.log(err));
