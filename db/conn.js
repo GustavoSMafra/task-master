b3bee817ffacba6aedf5fb357800da5eca7b1c09
GustavoSMafra <mafraguh@gmail.com>
2024-05-02 13:17:26 -0300

    Changed sequelize logic to work with .env db credentials

--- db/conn.js
+++ db/conn.js
@@ -1,9 +1,20 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new  Sequelize('task_node', 'root', 'root123', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+
+const sequelizeOptions = {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+}
+
+if (process.env.DB_SSL === 'true') {
+    sequelizeOptions.dialectOptions = {
+        ssl: true
+    };
+}
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, sequelizeOptions);
 
 try {
     sequelize.authenticate();
